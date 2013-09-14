20067d671f8259be71050898a682768f5476fc95
Matt Hayes <matt@mysterycommand.com>
2013-09-14 16:52:26 -0500

    Add methods to Field to matrix math. Update render loop. Draw two fields.

--- app/scripts/dynamics/Particle.js
+++ app/scripts/dynamics/Particle.js
@@ -14,9 +14,9 @@ define([
     'use strict';
 
     function Particle(ds, vx, vy) {
-        this.ds = ds || 0;
-        this.vx = vx || 0;
-        this.vy = vy || 0;
+        this.ods = this.ds = ds || 0;
+        this.ovx = this.vx = vx || 0;
+        this.ovy = this.vy = vy || 0;
     }
 
     Particle.prototype = Object.create(Object.prototype, {
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,23 +10,26 @@
 require([
 
     'jquery',
+    'dynamics/Field',
     'polyfill/window.requestAnimationFrame'
 
 ], function (
 
-    $
+    $,
+    Field
 
 ) {
 
     'use strict';
     if (window.$) { window.$.noConflict(true); }
     // if (window._) { window._.noConflict(); }
+    var IS_TOUCH = window.Modernizr.touch;
 
     $(function() {
 
         var main = document.getElementById('js-main');
         var canvas = document.getElementById('js-canvas');
-        var res = 8;
+        var res = (IS_TOUCH) ? 16 : 8;
         var w = canvas.width = Math.floor(main.offsetWidth / res);
         var h = canvas.height = Math.floor(main.offsetHeight / res);
 
@@ -35,10 +38,11 @@ require([
         canvas.style.width = displayW + 'px';
         canvas.style.height = displayH + 'px';
 
-        var ctx = canvas.getContext('2d');
+        var canvasCtx = canvas.getContext('2d');
+        canvasCtx.imageSmoothingEnabled = false;
         // console.log(main);
         // console.log(canvas);
-        // console.log(ctx);
+        // console.log(canvasCtx);
 
         var pointerDown = false;
         var ox, oy, x, y;
@@ -85,7 +89,10 @@ require([
         var requestId = null;
         var isRunning = false;
         var then, now, d;
-        var dx, dy, len, px, py, i;
+
+        var oldField = new Field(w, h);
+        var field;
+
         var render = function(time) {
             requestId = window.requestAnimationFrame(render);
 
@@ -94,12 +101,19 @@ require([
             d = now - then;
             then = now;
 
-            ctx.clearRect(0, 0, w, h);
-
-            ctx.fillStyle = 'grey';
-            ctx.fillRect(1, 1, w - 2, h - 2);
+            oldField.subLinearDensity(0.0025);
+            draw(oldField);
 
             if ( ! pointerDown) { return; }
+
+            field = new Field(w, h);
+            update(field);
+
+            oldField.addFieldDensities(field);
+        };
+
+        var dx, dy, len, px, py, i;
+        function update(field) {
             if ((0 <= ox && ox < displayW) &&
                 (0 <= oy && oy < displayH)) {
                 dx = x - ox;
@@ -111,19 +125,41 @@ require([
                     px = (((ox + dx * (i / len)) / displayW) * w) | 0;
                     py = (((oy + dy * (i / len)) / displayH) * h) | 0;
 
-                    ctx.fillStyle = 'rgba(0,0,0,0.5)';
-                    ctx.fillRect(px, py, 1, 1);
+                    field.setDensity(px, py, 0.1);
                 }
                 ox = x;
                 oy = y;
-
-                // ctx.fillStyle = hex;
-                // ctx.beginPath();
-                // ctx.arc(x / res, y / res, 10 / res, 0, PI2, false);
-                // ctx.fill();
-                // ctx.closePath();
             }
-        };
+        }
+
+        var buffer = document.createElement('canvas');
+        var bufferCtx = buffer.getContext('2d');
+        bufferCtx.imageSmoothingEnabled = false;
+        // This is the 'clampData' check. It passes in WebKit, so I'm not worrying about it.
+        // bufferData = bufferCtx.createImageData(w, h);
+        // bufferData.data[0] = 256;
+        // console.log(bufferData.data[0] > 255);
+
+        var bufferData, data, row, col, cel, k;
+        function draw(field) {
+            bufferData = bufferCtx.createImageData(w, h);
+            data = bufferData.data;
+            for (k = 0; k < data.length; ++k) { // set the whole buffer to 100% alpha white
+            // for (k = 3; k < data.length; k+=4) { // set the alpha channel to 100% alpha
+                data[k] = 255;
+            }
+
+            for (col = 0; col < w; ++col) {
+                for (row = 0; row < h; ++row) {
+                    cel = field.getDensity(col, row) * 255;
+                    data[4 * (row * w + col) + 0] -= cel;
+                    data[4 * (row * w + col) + 1] -= cel;
+                    data[4 * (row * w + col) + 2] -= cel;
+                }
+            }
+
+            canvasCtx.putImageData(bufferData, 0, 0);
+        }
 
         function start() {
             requestId = window.requestAnimationFrame(render);
@@ -132,11 +168,11 @@ require([
 
         function stop() {
             window.cancelAnimationFrame(requestId);
-            ctx.clearRect(0, 0, w, h);
+            // canvasCtx.clearRect(0, 0, w, h);
             isRunning = false;
         }
 
-        if (window.Modernizr.touch) {
+        if (IS_TOUCH) {
             start();
         } else {
             window.addEventListener('keyup', function(event) {
