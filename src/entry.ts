506f24e129abb8e67e80cbe9d4bdbd74cf3bd991
Matt Hayes <matt@mysterycommand.com>
2020-07-12 16:37:46 -0400

    refactor: collect input in the event handler and only draw in the animation loop

--- src/entry.ts
+++ src/entry.ts
@@ -14,35 +14,29 @@ const buffer = canvas.cloneNode() as HTMLCanvasElement;
 const bufferContext = buffer.getContext("2d")!;
 const bufferImageData = bufferContext.createImageData(w, h);
 const { data: bufferData } = bufferImageData;
-bufferData.fill(0);
 
 const pixel = (x: number, y: number) => y * w * 4 + x * 4;
 
-let maxDx = 0;
-let maxDy = 0;
+let input: {
+  x: number;
+  y: number;
+  dx: number;
+  dy: number;
+  ox: number;
+  oy: number;
+  len: number;
+};
 
 on(window, "mousemove", (event) => {
   if (!event.buttons) {
+    input = { x: 0, y: 0, dx: 0, dy: 0, ox: 0, oy: 0, len: 0 };
     return;
   }
 
   const { clientX: x, clientY: y, movementX: dx, movementY: dy } = event;
   const [ox, oy, len] = [x - dx, y - dy, max(1, (hypot(dx, dy) + 0.5) | 0)];
 
-  maxDx = max(abs(dx), maxDx);
-  maxDy = max(abs(dy), maxDy);
-
-  for (let i = 0; i < len; ++i) {
-    const px = (ox + dx * (i / len)) | 0;
-    const py = (oy + dy * (i / len)) | 0;
-
-    const offset = pixel(px, py);
-
-    bufferData[offset + 0] = 0;
-    bufferData[offset + 1] = min(abs(dx), 255);
-    bufferData[offset + 2] = min(abs(dy), 255);
-    bufferData[offset + 3] = 255;
-  }
+  input = { x, y, dx, dy, ox, oy, len };
 });
 
 const tick = (t: DOMHighResTimeStamp) => {
@@ -51,17 +45,23 @@ const tick = (t: DOMHighResTimeStamp) => {
   canvasContext.fillStyle = `hsl(${(t / 100) % 360}, 50%, 90%)`;
   canvasContext.fillRect(0, 0, w, h);
 
+  bufferData.fill(0);
+  for (let i = 0; i < input.len ?? 0; ++i) {
+    const px = (input.ox + input.dx * (i / input.len)) | 0;
+    const py = (input.oy + input.dy * (i / input.len)) | 0;
+
+    const offset = pixel(px, py);
+
+    bufferData[offset + 0] = 0;
+    bufferData[offset + 1] = min(abs(input.dx), 255);
+    bufferData[offset + 2] = min(abs(input.dy), 255);
+    bufferData[offset + 3] = 255;
+  }
+
   bufferContext.putImageData(bufferImageData, 0, 0);
   canvasContext.drawImage(buffer, 0, 0);
 
-  debug.innerText = JSON.stringify(
-    {
-      maxDx,
-      maxDy,
-    },
-    null,
-    2
-  );
+  debug.innerText = JSON.stringify({}, null, 2);
 };
 
 raf(tick);
