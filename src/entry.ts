610c3e910712b20b5ce09525fdc6da5167804c12
Matt Hayes <matt@mysterycommand.com>
2020-07-12 16:11:37 -0400

    feat: debug area, input handling, even helpers

--- src/entry.ts
+++ src/entry.ts
@@ -1,7 +1,11 @@
+import { on } from "./events";
+
+const { abs, hypot, max, min } = Math;
+const { innerWidth: w, innerHeight: h, requestAnimationFrame: raf } = window;
+
+const debug = document.querySelector("pre") as HTMLPreElement;
 const canvas = document.querySelector("canvas") as HTMLCanvasElement;
 const canvasContext = canvas.getContext("2d")!;
-
-const { innerWidth: w, innerHeight: h, requestAnimationFrame: raf } = window;
 
 canvas.width = w;
 canvas.height = h;
@@ -10,6 +14,36 @@ const buffer = canvas.cloneNode() as HTMLCanvasElement;
 const bufferContext = buffer.getContext("2d")!;
 const bufferImageData = bufferContext.createImageData(w, h);
 const { data: bufferData } = bufferImageData;
+bufferData.fill(0);
+
+const pixel = (x: number, y: number) => y * w * 4 + x * 4;
+
+let maxDx = 0;
+let maxDy = 0;
+
+on(window, "mousemove", (event) => {
+  if (!event.buttons) {
+    return;
+  }
+
+  const { clientX: x, clientY: y, movementX: dx, movementY: dy } = event;
+  const [ox, oy, len] = [x - dx, y - dy, max(1, (hypot(dx, dy) + 0.5) | 0)];
+
+  maxDx = max(abs(dx), maxDx);
+  maxDy = max(abs(dy), maxDy);
+
+  for (let i = 0; i < len; ++i) {
+    const px = (ox + dx * (i / len)) | 0;
+    const py = (oy + dy * (i / len)) | 0;
+
+    const offset = pixel(px, py);
+
+    bufferData[offset + 0] = 0;
+    bufferData[offset + 1] = min(abs(dx), 255);
+    bufferData[offset + 2] = min(abs(dy), 255);
+    bufferData[offset + 3] = 255;
+  }
+});
 
 const tick = (t: DOMHighResTimeStamp) => {
   raf(tick);
@@ -19,6 +53,15 @@ const tick = (t: DOMHighResTimeStamp) => {
 
   bufferContext.putImageData(bufferImageData, 0, 0);
   canvasContext.drawImage(buffer, 0, 0);
+
+  debug.innerText = JSON.stringify(
+    {
+      maxDx,
+      maxDy,
+    },
+    null,
+    2
+  );
 };
 
 raf(tick);
