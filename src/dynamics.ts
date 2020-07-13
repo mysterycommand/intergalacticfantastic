ecf6bbbfc0b871156518ca30782f5d1949628c52
Matt Hayes <matt@mysterycommand.com>
2020-07-13 19:16:47 -0400

    refactor: get it working with good ol' copy and paste

--- src/dynamics.ts
+++ src/dynamics.ts
@@ -1,254 +1,372 @@
-const { abs, max, min, sqrt } = Math;
+export class Fluid {
+  // private variables
+  _update: (field: Field) => {};
+  _render: (field: Field) => {};
+  _iterations = 10;
+  dt = 0.1;
+  _densities: number[];
+  _oDensities: number[];
+  _velocitiesX: number[];
+  _oVelocitiesX: number[];
+  _velocitiesY: number[];
+  _oVelocitiesY: number[];
+  _width: number;
+  _height: number;
+  _rowSize: number;
+  _size: number;
 
-export interface Input {
-  x: number;
-  y: number;
-  dx: number;
-  dy: number;
-  ox: number;
-  oy: number;
-  len: number;
-}
-
-export class Dynamics {
-  readonly cols: number;
-  readonly rows: number;
-  readonly size: number;
-
-  private aDensities: number[] = new Array(this.size).fill(0);
-  private bDensities: number[] = Array.from(this.aDensities);
-
-  private aVelocitiesX: number[] = new Array(this.size).fill(0);
-  private bVelocitiesX: number[] = Array.from(this.aVelocitiesX);
-
-  private aVelocitiesY: number[] = new Array(this.size).fill(0);
-  private bVelocitiesY: number[] = Array.from(this.aVelocitiesY);
-
-  private iterations: number = 10;
-  private ii: number = 1 / this.iterations;
-  private q: number = -0.5 / sqrt(this.w * this.h);
-  private hw: number = this.w / 2;
-  private hh: number = this.h / 2;
-  private iw: number = this.w * this.ii;
-  private ih: number = this.h * this.ii;
-
-  constructor(readonly w: number, readonly h: number) {
-    this.cols = w + 2;
-    this.rows = h + 2;
-    this.size = this.cols * this.rows;
+  // public/privelaged methods
+  setUpdateCallback(callback) {
+    this._update = callback;
   }
+  setRenderCallback = function (callback) {
+    this._render = callback;
+  };
 
-  public step(input: Input, data: Uint8ClampedArray) {
-    this.queryUi(input);
-
-    this.velocities(
-      this.aVelocitiesX,
-      this.aVelocitiesY,
-      this.bVelocitiesX,
-      this.bVelocitiesY
-    );
-    this.densities(
-      this.aDensities,
-      this.bDensities,
-      this.aVelocitiesX,
-      this.aVelocitiesY
-    );
-
-    this.render(data);
+  iterations() {
+    return this._iterations;
   }
-
-  private cell(x: number, y: number) {
-    return y * this.cols + x;
-  }
-
-  private queryUi(input: Input) {
-    this.bVelocitiesX.fill(0);
-    this.bVelocitiesY.fill(0);
-    this.bDensities.fill(0);
-
-    for (let i = 0; i < input.len ?? 0; ++i) {
-      const px = (input.ox + input.dx * (i / input.len)) | 0;
-      const py = (input.oy + input.dy * (i / input.len)) | 0;
-
-      const cell = this.cell(px, py);
-
-      this.bVelocitiesX[cell] = input.dx;
-      this.bVelocitiesY[cell] = input.dy;
-      this.bDensities[cell] = 25;
+  setIterations(i) {
+    if (0 < i && i <= 100) {
+      this._iterations = i;
     }
   }
 
-  private addTo(as: number[], bs: number[]) {
-    for (let i = 0; i < this.size; ++i) {
-      as[i] += bs[i] * this.ii;
+  setResolution(w, h) {
+    var res = w * h;
+
+    if (
+      !(1 < res && res < 1_000_000) ||
+      this._width === w ||
+      this._height === h
+    ) {
+      throw new Error(`res too high: ${res}`);
+    }
+
+    this._width = w;
+    this._height = h;
+    this._reset();
+  }
+
+  step() {
+    this._queryUI(this._oDensities, this._oVelocitiesX, this._oVelocitiesY);
+
+    this._velocityStep(
+      this._velocitiesX,
+      this._velocitiesY,
+      this._oVelocitiesX,
+      this._oVelocitiesY,
+      this.dt
+    );
+    this._densityStep(
+      this._densities,
+      this._oDensities,
+      this._velocitiesX,
+      this._velocitiesY,
+      this.dt
+    );
+
+    this._render(
+      new Field(
+        this._densities,
+        this._velocitiesX,
+        this._velocitiesY,
+        this._width,
+        this._height,
+        this._rowSize
+      )
+    );
+  }
+
+  // private methods
+  _reset() {
+    this._rowSize = this._width + 2;
+    this._size = (this._width + 2) * (this._height + 2);
+
+    this._densities = new Array(this._size);
+    this._oDensities = new Array(this._size);
+
+    this._velocitiesX = new Array(this._size);
+    this._oVelocitiesX = new Array(this._size);
+
+    this._velocitiesY = new Array(this._size);
+    this._oVelocitiesY = new Array(this._size);
+
+    for (var i = 0; i < this._size; i++) {
+      this._oDensities[i] = this._oVelocitiesX[i] = this._oVelocitiesY[
+        i
+      ] = this._densities[i] = this._velocitiesX[i] = this._velocitiesY[i] = 0;
     }
   }
 
-  private diffuse(as: number[], bs: number[]) {
-    for (let j = 1; j <= this.h; ++j) {
-      let index = j * this.cols;
-      ++index;
+  _queryUI(d, u, v) {
+    for (var i = 0; i < this._size; i++) {
+      d[i] = u[i] = v[i] = 0.0;
+    }
 
-      for (let i = 0; i < this.w; ++i) {
-        as[index] = bs[index];
-        ++index;
+    this._update(new Field(d, u, v, this._width, this._height, this._rowSize));
+  }
+
+  _velocityStep(u, v, u0, v0, dt) {
+    this._addFields(u, u0, dt);
+    this._addFields(v, v0, dt);
+
+    u = [u0, (u0 = u)][0];
+    v = [v0, (v0 = v)][0];
+
+    this._diffuse2(u, u0, v, v0, dt);
+    this._project(u, v, u0, v0);
+
+    u = [u0, (u0 = u)][0];
+    v = [v0, (v0 = v)][0];
+
+    this._advect(1, u, u0, u0, v0, dt);
+    this._advect(2, v, v0, u0, v0, dt);
+
+    this._project(u, v, u0, v0);
+  }
+
+  _densityStep(x, x0, u, v, dt) {
+    this._addFields(x, x0, dt);
+    this._diffuse(0, x0, x, dt);
+    this._advect(0, x, x0, u, v, dt);
+  }
+
+  _addFields(x, s, dt) {
+    for (var i = 0; i < this._size; ++i) {
+      x[i] += dt * s[i];
+    }
+  }
+
+  _linearSolve(b, x, x0, a, c) {
+    var i, j, k, lastRow, currentRow, nextRow, lastX, invC;
+
+    if (a === 0 && c === 1) {
+      for (j = 1; j <= this._height; ++j) {
+        currentRow = j * this._rowSize;
+        ++currentRow;
+        for (i = 0; i < this._width; ++i) {
+          x[currentRow] = x0[currentRow];
+          ++currentRow;
+        }
       }
-    }
-  }
-
-  private solve(xs: number[], ys: number[]) {
-    for (let k = 0; k < this.iterations; ++k) {
-      for (let j = 1; j <= this.h; ++j) {
-        let prevRowIndex = (j - 1) * this.cols;
-        let currRowIndex = j * this.cols;
-        let nextRowIndex = (j + 1) * this.cols;
-
-        let x = xs[currRowIndex];
-        ++currRowIndex;
-
-        for (let i = 1; i <= this.w; ++i) {
-          x = xs[currRowIndex] =
-            (ys[currRowIndex] +
-              (x +
-                xs[++currRowIndex] +
-                xs[++prevRowIndex] +
-                xs[++nextRowIndex])) *
-            0.25;
+    } else {
+      invC = 1 / c;
+      for (k = 0; k < this._iterations; ++k) {
+        for (j = 1; j <= this._height; ++j) {
+          lastRow = (j - 1) * this._rowSize;
+          currentRow = j * this._rowSize;
+          nextRow = (j + 1) * this._rowSize;
+          lastX = x[currentRow];
+          ++currentRow;
+          for (i = 1; i <= this._width; ++i) {
+            lastX = x[currentRow] =
+              (x0[currentRow] +
+                a * (lastX + x[++currentRow] + x[++lastRow] + x[++nextRow])) *
+              invC;
+          }
         }
       }
     }
   }
 
-  private project(axs: number[], ays: number[], bxs: number[], bys: number[]) {
-    for (let j = 1; j <= this.h; ++j) {
-      let prevRowIndex = (j - 1) * this.cols;
-      let currRowIndex = j * this.cols;
-      let nextRowIndex = (j + 1) * this.cols;
-
-      let prevIndex = currRowIndex - 1;
-      let nextIndex = currRowIndex + 1;
-
-      for (let i = 1; i <= this.w; ++i) {
-        bys[++currRowIndex] =
-          this.q *
-          (axs[++nextIndex] -
-            axs[++prevIndex] +
-            ays[++nextRowIndex] -
-            ays[++prevRowIndex]);
-        bxs[currRowIndex] = 0;
-      }
-    }
-
-    this.solve(bxs, bys);
-
-    for (let j = 1; j <= this.h; ++j) {
-      let prevRowIndex = (j - 1) * this.cols;
-      let currRowIndex = j * this.cols;
-      let nextRowIndex = (j + 1) * this.cols;
-
-      let prevIndex = currRowIndex - 1;
-      let currIndex = currRowIndex;
-      let nextIndex = currRowIndex + 1;
-
-      for (let i = 1; i <= this.w; ++i) {
-        axs[++currIndex] -= this.hw * (bxs[++nextIndex] - bxs[++prevIndex]);
-        ays[currIndex] -= this.hh * (bxs[++nextRowIndex] - bxs[++prevRowIndex]);
-      }
-    }
+  _diffuse(b, x, x0, dt) {
+    var a = 0;
+    this._linearSolve(b, x, x0, a, 1 + 4 * a);
   }
 
-  private advect(as: number[], bs: number[], xs: number[], ys: number[]) {
-    for (let j = 1; j <= this.h; ++j) {
-      let currRowIndex = j * this.cols;
+  _linearSolve2(x, x0, y, y0, a, c) {
+    var i, j, k, lastRow, currentRow, nextRow, lastX, lastY, invC;
 
-      for (let i = 1; i <= this.w; ++i) {
-        let x = min(max(0.5, i - this.iw * xs[++currRowIndex]), this.hw);
-        let y = min(max(0.5, j - this.ih * ys[currRowIndex]), this.hh);
-
-        let i0 = x | 0;
-        let i1 = i0 + 1;
-
-        let j0 = y | 0;
-        let j1 = j0 + 1;
-
-        let s1 = x - i0;
-        let s0 = 1 - s1;
-
-        let t1 = y - j0;
-        let t0 = 1 - t1;
-
-        let r1 = j0 * this.cols;
-        let r2 = j1 * this.cols;
-
-        as[currRowIndex] =
-          s0 * (t0 * bs[i0 + r1] + t1 * bs[i0 + r2]) +
-          s1 * (t0 * bs[i1 + r1] + t1 * bs[i1 + r2]);
+    if (a === 0 && c === 1) {
+      for (j = 1; j <= this._height; ++j) {
+        currentRow = j * this._rowSize;
+        ++currentRow;
+        for (i = 0; i < this._width; ++i) {
+          x[currentRow] = x0[currentRow];
+          y[currentRow] = y0[currentRow];
+          ++currentRow;
+        }
+      }
+    } else {
+      invC = 1 / c;
+      for (k = 0; k < this._iterations; ++k) {
+        for (j = 1; j <= this._height; ++j) {
+          lastRow = (j - 1) * this._rowSize;
+          currentRow = j * this._rowSize;
+          nextRow = (j + 1) * this._rowSize;
+          lastX = x[currentRow];
+          lastY = y[currentRow];
+          ++currentRow;
+          for (i = 1; i <= this._width; ++i) {
+            lastX = x[currentRow] =
+              (x0[currentRow] +
+                a * (lastX + x[currentRow] + x[lastRow] + x[nextRow])) *
+              invC;
+            lastY = y[currentRow] =
+              (y0[currentRow] +
+                a * (lastY + y[++currentRow] + y[++lastRow] + y[++nextRow])) *
+              invC;
+          }
+        }
       }
     }
   }
 
-  private velocities(
-    axs: number[],
-    ays: number[],
-    bxs: number[],
-    bys: number[]
-  ) {
-    this.addTo(axs, bxs);
-    this.addTo(ays, bys);
-
-    // swap
-    [axs, bxs] = [bxs, axs];
-    [ays, bys] = [bys, ays];
-
-    // diffuse
-    this.diffuse(axs, bxs);
-    // diffuse
-    this.diffuse(ays, bys);
-    // project
-    this.project(axs, ays, bxs, bys);
-
-    // swap
-    [axs, bxs] = [bxs, axs];
-    [ays, bys] = [bys, ays];
-
-    // advect
-    this.advect(axs, bxs, bxs, bys);
-    // advect
-    this.advect(ays, bys, bxs, bys);
-    // project
-    this.project(axs, ays, bxs, bys);
+  _diffuse2(x, x0, y, y0, dt) {
+    var a = 0;
+    this._linearSolve2(x, x0, y, y0, a, 1 + 4 * a);
   }
 
-  private densities(
-    ads: number[],
-    bds: number[],
-    axs: number[],
-    ays: number[]
-  ) {
-    this.addTo(ads, bds);
+  _advect(b, d, d0, u, v, dt) {
+    var i, j, pos, x, y, i0, i1, j0, j1, s0, s1, t0, t1, row1, row2;
 
-    // swap
-    [ads, bds] = [bds, ads];
+    var Wdt0 = dt * this._width;
+    var Hdt0 = dt * this._height;
+    var Wp5 = this._width + 0.5;
+    var Hp5 = this._height + 0.5;
 
-    // diffuse
-    this.diffuse(ads, bds);
-    // advect
-    this.advect(ads, bds, axs, ays);
-  }
+    for (j = 1; j <= this._height; ++j) {
+      pos = j * this._rowSize;
+      for (i = 1; i <= this._width; ++i) {
+        x = i - Wdt0 * u[++pos];
+        y = j - Hdt0 * v[pos];
 
-  private render(data: Uint8ClampedArray) {
-    data.fill(0);
+        if (x < 0.5) {
+          x = 0.5;
+        } else if (x > Wp5) {
+          x = Wp5;
+        }
 
-    for (let i = 0; i < this.w; ++i) {
-      for (let j = 0; j < this.h; ++j) {
-        const offset = 4 * (j * this.w + i);
-        const cell = this.cell(i, j);
+        i0 = x | 0;
+        i1 = i0 + 1;
 
-        // data[offset + 0] = 0;
-        // data[offset + 1] = 0;
-        // data[offset + 2] = 0;
-        data[offset + 3] = this.aDensities[cell];
+        if (y < 0.5) {
+          y = 0.5;
+        } else if (y > Hp5) {
+          y = Hp5;
+        }
+
+        j0 = y | 0;
+        j1 = j0 + 1;
+
+        s1 = x - i0;
+        s0 = 1 - s1;
+
+        t1 = y - j0;
+        t0 = 1 - t1;
+
+        row1 = j0 * this._rowSize;
+        row2 = j1 * this._rowSize;
+
+        d[pos] =
+          s0 * (t0 * d0[i0 + row1] + t1 * d0[i0 + row2]) +
+          s1 * (t0 * d0[i1 + row1] + t1 * d0[i1 + row2]);
       }
     }
+  }
+
+  _project(u, v, p, div) {
+    var i,
+      j,
+      prevRow,
+      currentRow,
+      nextRow,
+      prevValue,
+      nextValue,
+      prevPos,
+      currentPos,
+      nextPos;
+
+    var h = -0.5 / Math.sqrt(this._width * this._height);
+
+    for (j = 1; j <= this._height; ++j) {
+      prevRow = (j - 1) * this._rowSize;
+      currentRow = j * this._rowSize;
+      nextRow = (j + 1) * this._rowSize;
+
+      prevValue = currentRow - 1;
+      nextValue = currentRow + 1;
+
+      for (i = 1; i <= this._width; ++i) {
+        div[++currentRow] =
+          h * (u[++nextValue] - u[++prevValue] + v[++nextRow] - v[++prevRow]);
+        p[currentRow] = 0;
+      }
+    }
+
+    this._linearSolve(0, p, div, 1, 4);
+
+    var halfWidth = 0.5 * this._width,
+      halfHeight = 0.5 * this._height;
+
+    for (j = 1; j <= this._height; ++j) {
+      prevPos = j * this._rowSize - 1;
+      currentPos = j * this._rowSize;
+      nextPos = j * this._rowSize + 1;
+
+      prevRow = (j - 1) * this._rowSize;
+      currentRow = j * this._rowSize;
+      nextRow = (j + 1) * this._rowSize;
+
+      for (i = 1; i <= this._width; ++i) {
+        u[++currentPos] -= halfWidth * (p[++nextPos] - p[++prevPos]);
+        v[currentPos] -= halfHeight * (p[++nextRow] - p[++prevRow]);
+      }
+    }
+  }
+}
+
+/**
+ * This is a private constructor that's used to pass state from the UI, and to the renderer.
+ * @param {Array} densities   An array of all the densities in the field.
+ * @param {Array} velocitiesX An array of all the x velocities in the field.
+ * @param {Array} velocitiesY An array of all the y velocities in the field.
+ */
+class Field {
+  constructor(
+    private _densities: number[],
+    private _velocitiesX: number[],
+    private _velocitiesY: number[],
+    private _width: number,
+    private _height: number,
+    private _rowSize: number
+  ) {}
+  // Just exposing the fields here rather than using accessors is a
+  // measurable win during display (maybe 5%) but makes the code ugly.
+
+  // function key(x, y) { return (x + 1) + (y + 1) * _rowSize; } // The original way.
+  key(x, y) {
+    return x + 1 + (y + 1) * this._rowSize;
+  } // The way that works.
+
+  setDensity(x, y, density) {
+    this._densities[this.key(x, y)] = density;
+  }
+
+  getDensity(x, y) {
+    return this._densities[this.key(x, y)];
+  }
+
+  // this.setVelocity = function(x, y, xv, yv) { // The original way.
+  setVelocity(x, y, velocityX, velocityY) {
+    // The way that works.
+    this._velocitiesX[this.key(x, y)] = velocityX;
+    this._velocitiesY[this.key(x, y)] = velocityY;
+  }
+
+  getXVelocity(x, y) {
+    return this._velocitiesX[this.key(x, y)];
+  }
+
+  getYVelocity(x, y) {
+    return this._velocitiesY[this.key(x, y)];
+  }
+
+  width() {
+    return this._width;
+  }
+  height() {
+    return this._height;
   }
 }
--- src/entry.ts
+++ src/entry.ts
@@ -1,4 +1,4 @@
-import { Input, Dynamics } from "./dynamics";
+import { Fluid } from "./dynamics";
 import { on } from "./events";
 
 const { hypot, max } = Math;
@@ -17,9 +17,8 @@ const bufferImageData = bufferContext.createImageData(w, h);
 const { data: bufferData } = bufferImageData;
 
 // const pixel = (x: number, y: number) => y * w * 4 + x * 4;
-const dynamics = new Dynamics(w, h);
 
-let input: Input = { x: 0, y: 0, dx: 0, dy: 0, ox: 0, oy: 0, len: 0 };
+let input = { x: 0, y: 0, dx: 0, dy: 0, ox: 0, oy: 0, len: 0 };
 
 on(window, "mousemove", (event) => {
   if (!event.buttons) {
@@ -33,16 +32,37 @@ on(window, "mousemove", (event) => {
   input = { x, y, dx, dy, ox, oy, len };
 });
 
+const fluid = new Fluid();
+fluid.setUpdateCallback((f) => {
+  for (let i = 0; i < input.len; ++i) {
+    const px = (input.ox + input.dx * (i / input.len)) | 0;
+    const py = (input.oy + input.dy * (i / input.len)) | 0;
+
+    f.setVelocity(px, py, input.dx, input.dy);
+    f.setDensity(px, py, 25);
+  }
+});
+fluid.setRenderCallback((f) => {
+  bufferData.fill(0);
+
+  for (let x = 0; x < w; ++x) {
+    for (let y = 0; y < h; ++y) {
+      bufferData[4 * (y * w + x) + 3] = f.getDensity(x, y) * 255;
+    }
+  }
+
+  bufferContext.putImageData(bufferImageData, 0, 0);
+  canvasContext.drawImage(buffer, 0, 0);
+});
+fluid.setResolution(w, h);
+
 const tick = (t: DOMHighResTimeStamp) => {
   raf(tick);
 
   canvasContext.fillStyle = `hsl(${(t / 100) % 360}, 50%, 90%)`;
   canvasContext.fillRect(0, 0, w, h);
 
-  dynamics.step(input, bufferData);
-
-  bufferContext.putImageData(bufferImageData, 0, 0);
-  canvasContext.drawImage(buffer, 0, 0);
+  fluid.step();
 
   debug.innerText = JSON.stringify({}, null, 2);
 };
